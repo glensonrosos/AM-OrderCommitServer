54fb5f8f265ed83c4175ffd279181b8b48caa4c4
Glenson Rosos <glensonixr21@gmail.com>
2023-09-22 14:50:32 +0800

    call distpact every 60 seconds

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(cors());
 
 
 const PORT = process.env.PORT || 5000;
-const CONNECTION_URL = "mongodb://localhost:27017"
+const CONNECTION_URL = "mongodb://127.0.0.1:27017/ordercommitment"
 
 
 mongoose.connect(CONNECTION_URL,{useNewUrlParser:true, useUnifiedTopology:true})
